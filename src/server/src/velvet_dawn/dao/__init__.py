2ad8c8212d7f91871feab67a7ca778f57fa99ad6
Peter Hurt <peter.hurt@sky.com>
2023-01-04 13:06:39 +0000

    feat: Add option to remove game.db on launch, if it exists

--- src/server/src/velvet_dawn/dao/__init__.py
+++ src/server/src/velvet_dawn/dao/__init__.py
@@ -2,8 +2,28 @@ import time
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from pathlib import Path
+from os import remove
 
 import velvet_dawn
+
+db_path=Path(__file__).parent.parent.parent / "instance" / "game.db"
+if db_path.exists():
+    i = 0
+    while i == 0:
+        resume = input("A game alredy exists. Would you like to resume or start fresh?\nResume game = 0 (Default)\nStart Fresh = 1\nEnter your choice here: ")
+        if resume == "1":
+            print("Creating a new game...")
+            remove(db_path)
+            i = 1
+        elif resume in ("0", ""):
+            print("Resuming existing game...")
+            i = 1
+        else:
+            print("\nImproper answer! Please choose from the available options or hit 'return' to accept the default option.\n\n")
+    del i
+else:
+    print("Existing game not found, creating a new game...")
 
 db = SQLAlchemy()
 app = Flask(__name__)
